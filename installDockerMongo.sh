1b5b414a43cf15c2ea2f8848d1665b5272d9a368
joao <joaocfariaf@gmail.com>
2020-06-14 12:56:23 -0300

    sudo add-apt-repository  command fixed

--- installDockerMongo.sh
+++ installDockerMongo.sh
@@ -7,7 +7,10 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo docker pull mongo
