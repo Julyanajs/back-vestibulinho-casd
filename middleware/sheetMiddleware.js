f24eae0eb93c5250b2d75d31ec21eb46d1c79573
Folksz <pedrohen.fc@gmail.com>
2020-06-18 13:01:52 -0300

    adicao de salas e correcao import e export excel

--- middleware/sheetMiddleware.js
+++ middleware/sheetMiddleware.js
@@ -3,17 +3,19 @@ const xlsx = require("xlsx");
 const additionalInfoService = require('../services/additionalInfoService');
 const candidateStatusService = require('../services/candidateStatusService');
 const CandidateService = require('../services/candidateService');
+const RoomService = require('../services/roomService');
 
 
 module.exports.SaveCandidatesList = async (req,res,next) => {
-	var wb=xlsx.readFile(__dirname+'/input.xlsx',{cellDates:true});
-   var ws=wb.sheets["sheet1"];
+	var wb=xlsx.readFile(__dirname+'/input_candidates.xlsx',{cellDates:true});
+   var ws=wb.Sheets["Sheet1"];
    var data=xlsx.utils.sheet_to_json(ws);
-   data.foreach(function(p) {
+   console.log(data)
+   data.forEach(function(p) {
     p.address= {
-	"additionaladdress":p.additionaladdress,
+	"additionalAddress":p.additionalAddress,
 	"street":p.street,
-	"numberstreet":p.numberstreet,
+	"numberStreet":p.numberStreet,
 	"neighborhood":p.neighborhood,
 	"city":p.city,
 	"state":p.state,
@@ -27,39 +29,66 @@ module.exports.SaveCandidatesList = async (req,res,next) => {
 	delete p.cep;
    });
 	req.data=data;
+	
+   next();
+}
+
+module.exports.SaveRoomList = async (req,res,next) => {
+	var wb=xlsx.readFile(__dirname+'/input_room.xlsx',{cellDates:true});
+   var ws=wb.Sheets["Sheet1"];
+   var data=xlsx.utils.sheet_to_json(ws);
+	req.data=data;
+	console.log(data)
    next();
 }
 
 //oq eu fiz
 module.exports.preSaveAdditionalInfoList = async (req,res,next) => {
-	console.log("additional list");
    req.data =  await req.data.map(async p=>{
       const additionalinfo =await additionalinfoservice.create(p);
-      console.log(additionalinfo);
       p.additionalInfo=  additionalInfo._id	
    });
-   console.log("Fim ADditional Info");
    next();
 }  
 
 module.exports.preSaveCandidateStatusList = async (req,res,next) => {
-console.log("Candidate Status");
    req.data =  await req.data.map(async p=>{
       const candidateStatus =await candidateStatusService.create(p.candidateStatus);
-      //console.log(additionalInfo);
       p.candidateStatus=  candidateStatus._id
 	   });
-   console.log("Fim Candidate Status");
    next();
 }
 
 module.exports.exportExcel = async (req,res,next) => {
    const candidate = await CandidateService.getAll();
+   console.log(candidate)
+   console.log(candidate[0].additionalInfo.street)
+   candidate.forEach(function(p) {
+	p.gender=p.additionalInfo.gender;
+	p.school=p.additionalInfo.school;
+	p.kindSchool=p.additionalInfo.kindSchool;
+	p.ifSpecialNecessity=p.additionalInfo.ifSpecialNecessity;
+	p.phone1=p.additionalInfo.phone1;
+	p.kinship=p.additionalInfo.kinship;
+	p.relativeName=p.additionalInfo.relativeName;
+	p.schooling=p.additionalInfo.schooling;
+	p.wayPS=p.additionalInfo.wayPS;
+	p.birthDate=p.additionalInfo.birthDate;
+	p.additionalAddress=p.additionalInfo.address.additionalAddress;
+	p.street=p.additionalInfo.address.street;
+	p.numberStreet=p.additionalInfo.address.numberStreet;
+	p.neighborhood=p.additionalInfo.address.neighborhood;
+	p.city=p.additionalInfo.address.city;
+   p.state=p.additionalInfo.address.state;
+   p.cep=p.additionalInfo.address.cep;
+   
+   });
+
+	
    var newWB=xlsx.utils.book_new(); 
    var newWS=xlsx.utils.json_to_sheet(candidate);
-
    xlsx.utils.book_append_sheet(newWB,newWS,"OUTPUT");
-   const pathXlsx = __dirname + "/../public/Output.xlsx";
+   const pathXlsx = __dirname + "/../public/Output_Candidates.xlsx";
    await xlsx.writeFile(newWB,pathXlsx);
    res.download(pathXlsx, (err) => {
       if(err)
@@ -68,6 +97,24 @@ module.exports.exportExcel = async (req,res,next) => {
          next();
    })
 }
+
+
+module.exports.exportExcelRoom = async (req,res,next) => {
+   const room = await RoomService.getAll();
+   console.log(room);
+   var newWB=xlsx.utils.book_new(); 
+   var newWS=xlsx.utils.json_to_sheet(room);
+  xlsx.utils.book_append_sheet(newWB,newWS,"OUTPUT");
+   const pathXlsx = __dirname + "/../public/Output_room.xlsx";
+   await xlsx.writeFile(newWB,pathXlsx);
+   res.download(pathXlsx, (err) => {
+      if(err)
+         console.log(err);
+      else
+         next();
+   })
+}
+
 
 
 
